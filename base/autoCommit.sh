840a372ff6b2129e7e39bd3a862e3903bbdbcedc
Heeming <purete27@gmail.com>
2021-05-19 08:53:18 +0900

    [base] Update file:autoCommit.sh
    Delete echo command. Add sleep command for working more stably

--- base/autoCommit.sh
+++ base/autoCommit.sh
@@ -1,12 +1,18 @@
-echo "Checking for uncommitted changes in the git working tree"
+#!/bin/bash
+while :
+do
+  #echo "Checking for uncommitted changes in the git working tree"
 
-if ! git diff --quiet
-then
-  git checkout auto-commit
-  git add .
-  git commit -m "Auto Commit"
-  git push -u origin auto-commit
+  if ! git diff --quiet
+  then
+    git --no-pager checkout auto-commit
+    git --no-pager add .
+    git --no-pager commit -m "Auto Commit"
+    git --no-pager push -u origin auto-commit
 
-else
-  echo "Working tree clean. Nothing to commmit."
-fi
+  #else
+    #echo "Working tree clean. Nothing to commmit."
+  fi
+
+  sleep 60
+done
