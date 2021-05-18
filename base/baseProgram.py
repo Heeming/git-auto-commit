8b6a71476765fe0713b2ee20043971112326669a
Heeming <purete27@gmail.com>
2021-05-19 08:57:24 +0900

    [base] Update file:baseProgram.py
    Add code for calling additional shell script and simple terminal interface

--- base/baseProgram.py
+++ base/baseProgram.py
@@ -1,14 +1,46 @@
 import subprocess
+import time
 
-autoCommit = str(input('do autoCommit?[y/n] '))
+choice = 0
 
-if autoCommit == "y" or autoCommit == "Y":
-    subprocess.call(['sh', './autoCommit.sh'])
+while choice != 5:
+    print("Menu")
+    print("1. New")
+    print("2. Continue")
+    print("3. Git add file")
+    print("4. Git push to branch")
+    print("5. Exit")
+    choice = int(input(">> "))
 
-pushToMaster = str(input('push to other branch?[y/n] '))
-
-if pushToMaster == 'y' or pushToMaster == 'Y':
-    branch = str(input("Where to push?(branch_name) "))
-    msg = str(input("Write commit message: "))
+    if choice == 1:
+        subprocess.call(['sh', './setting.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
     
-    subprocess.call(['sh', './userCommit.sh', branch, msg])
+    elif choice == 2:
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
+    
+    elif choice == 3:
+        subprocess.call(['bash', './killProcess.sh'])
+
+        filename = str(input("What file to add?(file_name) "))
+        subprocess.call(['sh', './addFile.sh', filename])
+
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
+
+    elif choice == 4:
+        subprocess.call(['bash', './killProcess.sh'])
+        branch = str(input("Where to push?(branch_name) "))
+        msg = str(input("Write commit message: "))
+        
+        subprocess.call(['sh', './userCommit.sh', branch, msg])
+
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
+
+    elif choice == 5:
+        subprocess.call(['bash', './killProcess.sh'])
+
+    else:
+        print("Wrong Input! Please input again")
