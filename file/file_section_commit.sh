09a123ca0d4c17e94bf4fb061890f3f12f26455a
HyojinKim <gywls00100@gmail.com>
2021-05-18 18:52:09 +0900

    [file] Create specify file section.sh

---
+++ file/file_section_commit.sh
@@ -0,0 +1,8 @@
+echo "Specify filesection"
+
+file_section_start = "$FILE_SECTION_START"
+file_section_finish = "$FILE_SECTION_FINISH"
+
+#git add $FILE_SECTION_START to $FILE_SECTION_FINISH ??
+
+# 특정 구간 추적하는 것 어떻게 하지?
