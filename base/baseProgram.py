0202afc8cb4b2fca4718c9b308e60c5dfebd19a2
sanggae4133 <sanggae4133@naver.com>
2021-05-19 18:22:54 +0900

    issue #3 fixed

--- base/baseProgram.py
+++ base/baseProgram.py
@@ -1,15 +1,17 @@
 import subprocess
 import time
+import os
 
 choice = 0
 
-while choice != 5:
+while choice != 6:
     print("Menu")
     print("1. New")
     print("2. Continue")
-    print("3. Git add file")
-    print("4. Git push to branch")
-    print("5. Exit")
+    print("3. Error Backup mode")
+    print("4. Git add file")
+    print("5. Git push to branch")
+    print("6. Exit")
     choice = int(input(">> "))
 
     if choice == 1:
@@ -19,8 +21,27 @@ while choice != 5:
     elif choice == 2:
         subprocess.call(['sh', './continue.sh'])
         subprocess.call(['sh', './autoCommitProcess.sh'])
-    
+
     elif choice == 3:
+        path = "./code/"
+        file_list = os.listdir(path)
+
+        py_list = [file for file in file_list if file.endswith(".py")]
+        #c_list = [file for file in file_list if file.endswith(".c")]
+        #java_list = [file for file in file_list if file.endswith(".java")]
+
+        for i in range(len(py_list)) :
+            try :
+                subprocess.check_output( ['python', path + py_list[0]], universal_newlines=True )
+            except Exception as ex:
+                branch = str("error")
+                msg = str(ex)
+
+                subprocess.call(['sh', './continue.sh'])
+                subprocess.call(['sh', './autoCommitProcess.sh'])
+        
+    
+    elif choice == 4:
         subprocess.call(['bash', './killProcess.sh'])
 
         filename = str(input("What file to add?(file_name) "))
@@ -29,7 +50,7 @@ while choice != 5:
         subprocess.call(['sh', './continue.sh'])
         subprocess.call(['sh', './autoCommitProcess.sh'])
 
-    elif choice == 4:
+    elif choice == 5:
         subprocess.call(['bash', './killProcess.sh'])
         branch = str(input("Where to push?(branch_name) "))
         msg = str(input("Write commit message: "))
@@ -39,7 +60,7 @@ while choice != 5:
         subprocess.call(['sh', './continue.sh'])
         subprocess.call(['sh', './autoCommitProcess.sh'])
 
-    elif choice == 5:
+    elif choice == 6:
         subprocess.call(['bash', './killProcess.sh'])
 
     else:
