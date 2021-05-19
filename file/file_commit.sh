1d7a8d50bc86f15103e9f7eaac6e1d709e408619
HyojinKim <gywls00100@gmail.com>
2021-05-20 02:17:36 +0900

    [file] Modify file: specify file

--- file/file_commit.sh
+++ file/file_commit.sh
@@ -2,4 +2,13 @@ echo "Specify file"
 
 file_name = "$1"
 
-git add $file_name
+if ! git diff $file_name --quiet
+then
+  git checkout auto-commit
+  git add $file_name
+  git commit -m "Auto Commit: tracking only $file_name"
+  git push -u origin auto-commit
+
+else
+  echo "Working tree clean. Nothing to commmit."
+fi
