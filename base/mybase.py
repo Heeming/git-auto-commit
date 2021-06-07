e4e1a79989e458832d992d5575c1b03fd5d3eae2
sanggae4133 <sanggae4133@naver.com>
2021-06-08 03:12:23 +0900

    Auto Commit

--- base/mybase.py
+++ base/mybase.py
@@ -21,10 +21,11 @@ print(py_list)
 for i in range(len(py_list)) :
     try :
         print("&&")
-        print("11\n", subprocess.check_output( ['python', path + py_list[i]], universal_newlines=True), "\n11" )
+        print("11\n", subprocess.check_output( ['python', path + py_list[i]], universal_newlines=True, stderr=subprocess.PIPE), "\n11" )
         print("&&")
         
     except Exception as ex:
+
         branch = str("error")
         msg = str(ex)
 
