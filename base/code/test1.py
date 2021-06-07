f7868428a21902c5c5c4c29fadc09e4325f87446
sanggae4133 <sanggae4133@naver.com>
2021-06-08 02:02:58 +0900

    Auto Commit

--- base/code/test1.py
+++ base/code/test1.py
@@ -1,3 +1,4 @@
 print("This is test Python source code")
 print("This is test Python source code")
 print("This is test Python source code")
+a
---
+++ base/mybase.py
@@ -0,0 +1,28 @@
+import subprocess
+import time
+import os
+
+path = "./code/"
+file_list = os.listdir(path)
+
+print(file_list)
+
+py_list = [file for file in file_list if file.endswith(".py")]
+print(py_list)
+
+#c_list = [file for file in file_list if file.endswith(".c")]
+#java_list = [file for file in file_list if file.endswith(".java")]
+
+
+for i in range(len(py_list)) :
+    try :
+        print(subprocess.check_output( ['python', path + py_list[i]], universal_newlines=True) )
+        
+    except Exception as ex:
+        branch = str("error")
+        msg = str(ex)
+
+        print("commit")
+        
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
