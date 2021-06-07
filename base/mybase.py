ccfc554067e5a38929b3c0c5a78642c89d29dce8
sanggae4133 <sanggae4133@naver.com>
2021-06-08 04:22:54 +0900

    Auto Commit

--- base/mybase.py
+++ base/mybase.py
@@ -12,26 +12,38 @@ print(file_list)
 py_list = [file for file in file_list if file.endswith(".py")]
 print(py_list)
 
-#c_list = [file for file in file_list if file.endswith(".c")]
-#java_list = [file for file in file_list if file.endswith(".java")]
+c_list = [file for file in file_list if file.endswith(".c")]
+java_list = [file for file in file_list if file.endswith(".java")]
 
 
-massage = ''
 
 for i in range(len(py_list)) :
-    try :
-        print("&&")
-        massage = subprocess.check_output( ['python', path + py_list[i]], universal_newlines=True, stderr=subprocess.PIPE)
-        print("&&")
-        
-    except Exception as ex:
-        print(massage)
-        print(subprocess.Popen.communicate())
-        branch = str("error")
-        msg = str(ex)
+    p = subprocess.Popen(['python', path + py_list[i]], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    print(err)
+    if err == '':
+        pass
+    else:
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
+'''
+for i in range(len(c_list)) :
+#    p = subprocess.Popen(['python', path + py_list[i]], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
 
-        print("!!!!!!!!!!!commit")
-        print(ex)
-        
-        #subprocess.call(['sh', './continue.sh'])
-        #subprocess.call(['sh', './autoCommitProcess.sh'])
+    if err == '':
+        pass
+    else:
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
+
+for i in range(len(java_list)) :
+#    p = subprocess.Popen(['python', path + py_list[i]], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    if err == '':
+        pass
+    else:
+        subprocess.call(['sh', './continue.sh'])
+        subprocess.call(['sh', './autoCommitProcess.sh'])
+        '''
