11b8c19638e2bc0ef465e24e2a7da677ae0c04a6
Heeming <purete27@gmail.com>
2021-06-09 09:23:01 +0900

    [gui] Fix typo in file:deleteBranch.sh

--- base/deleteBranch.sh
+++ base/deleteBranch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 path="$1"  
-
+branch="$2"
 cd $path
+
+
 
 if ! git diff --quiet
 then
 
-    git --no-pager checkout auto-commit
-    git --no-pager add .
-    git --no-pager commit -m "Auto Commit"
-    git --no-pager push -u origin auto-commit
+    git checkout auto-commit
+    git add .
+    git commit -m "Auto Commit"
+    git push -u origin auto-commit
 
     # echo "Last commit is not merged."
     # echo -e "Where to push?(branch_name) \c"
@@ -17,7 +19,9 @@ then
     # echo -e "Write commit message: \c"
     # read -a message
 
-    branch="$2"
+    git checkout $branch
+    git branch -D auto-commit
+    git push origin $branch
 
     git checkout $branch
     # git merge --squash auto-commit
@@ -25,18 +29,20 @@ then
     # git push -u origin master
 
     git push origin --delete auto-commit
-    git branch -D auto-commit
 
 else
-    branch="$1"
     # message="$2"
 
     git checkout $branch
+    git branch -D auto-commit
+
+
+    git checkout $branch
     # git merge --squash auto-commit
     # git commit -m "${message[@]}"
     # git push -u origin master
 
     git push origin --delete auto-commit
-    git branch -D auto-commit
+
 
 fi
