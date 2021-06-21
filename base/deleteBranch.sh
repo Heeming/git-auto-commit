6f68defd289ca5cb24e387459209ff8548321ab0
Heeming <purete27@gmail.com>
2021-06-21 09:52:10 +0900

    [gui] Delete duplicate or unnecessary files

--- base/deleteBranch.sh
+++ base/deleteBranch.sh
@@ -3,8 +3,6 @@ path="$1"
 branch="$2"
 cd $path
 
-
-
 if ! git diff --quiet
 then
 
@@ -13,36 +11,20 @@ then
     git commit -m "Auto Commit"
     git push -u origin auto-commit
 
-    # echo "Last commit is not merged."
-    # echo -e "Where to push?(branch_name) \c"
-    # read branch
-    # echo -e "Write commit message: \c"
-    # read -a message
-
     git checkout $branch
     git branch -D auto-commit
     git push origin $branch
 
     git checkout $branch
-    # git merge --squash auto-commit
-    # git commit -m "${message[@]}"
-    # git push -u origin master
 
     git push origin --delete auto-commit
 
 else
-    # message="$2"
-
     git checkout $branch
     git branch -D auto-commit
 
 
     git checkout $branch
-    # git merge --squash auto-commit
-    # git commit -m "${message[@]}"
-    # git push -u origin master
 
     git push origin --delete auto-commit
-
-
 fi
--- base/error_autoCommitProcess.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sh error_autoCommit.sh &
