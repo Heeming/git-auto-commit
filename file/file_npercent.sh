5f8f810f968ff317cc6929f083324927afacbc14
HyojinKim <gywls00100@gmail.com>
2021-06-21 11:43:32 +0900

    [file] Fix type error

--- file/file_npercent.sh
+++ file/file_npercent.sh
@@ -7,18 +7,17 @@ do
   echo "Checking file change for $n%"
 
   diff_msg=`git diff --stat $filename`
-  FILE_ROW_COUNT=$(cat $filename| wc -m) # 전체 줄 수 
-  change_line=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 줄 수 
-  change=`expr $change_line \* 100` # 변경된 줄 수 / 전체 줄 수 
-  change_percent=`expr $change / $FILE_ROW_COUNT` # percent = 변경된 줄 수 / 전체 줄 수 * 100
-  echo "전체 글자 수 : $FILE_ROW_COUNT"
-  echo "변경된 글자 수 : $change_line"
-  echo "변경 : $change"
+  total_char=$(cat $filename| wc -m) # 전체 문자 수 
+  changes_char=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 문자 수 
+  changes=`expr $changes_char \* 100` # 변경된 문자 수 * 100
+  change_percent=`expr $changes / $total_char` # percent = 변경된 문자 수 / 전체 문자 수 * 100
+  echo "전체 문자 수 : $total_char"
+  echo "변경된 문자 수 : $changes_char"
+  echo "변경 : $changes"
   echo "변경된 퍼센트 : $change_percent"
 
   # if ! git diff --quiet
-  if [! git diff --quiet] && [$change_percent -gt $n]
-  then
+  if [ "$change_percent" -gt "$n" ];then
     git checkout auto-commit
     git add $filename
     git commit -m "Auto Commit: More than $n percent change detected."
