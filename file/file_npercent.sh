21a6a4464ff7751d43556ad36cfb6d2eb075eaeb
HyojinKim <gywls00100@gmail.com>
2021-06-21 12:23:58 +0900

    [file] Modify char to line

--- file/file_npercent.sh
+++ file/file_npercent.sh
@@ -7,12 +7,12 @@ do
   echo "Checking file change for $n%"
 
   diff_msg=`git diff --stat $filename`
-  total_char=$(cat $filename| wc -m) # 전체 문자 수 
-  changes_char=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 문자 수 
-  changes=`expr $changes_char \* 100` # 변경된 문자 수 * 100
-  change_percent=`expr $changes / $total_char` # percent = 변경된 문자 수 / 전체 문자 수 * 100
-  echo "전체 문자 수 : $total_char"
-  echo "변경된 문자 수 : $changes_char"
+  total_line=$(cat $filename| wc -l) # 전체 줄 수 
+  changes_line=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 줄 수 
+  changes=`expr $changes_line \* 100` # 변경된 줄 수 * 100
+  change_percent=`expr $changes / $total_char` # percent = 변경된 줄 수 / 전체 줄 수 * 100
+  echo "전체 줄 수 : $total_line"
+  echo "변경된 줄 수 : $changes_line"
   echo "변경 : $changes"
   echo "변경된 퍼센트 : $change_percent"
 
